f784f84fe9d9688157a9fdf93aff322adf9243d1
misunders2d <2djohar@gmail.com>
2025-06-18 16:55:33 +0300

    moving image and sku properties to a separate file

---
+++ image_links.py
@@ -0,0 +1,21 @@
+product_details = [
+    {
+        "skus":["BedSheetSet-King-Light-Gray-FBA","M-BEDSHEETSET-K-LIGHT-GRAY-PAK","M-BEDSHEETSET-K-LIGHT-GRAY-CMB"],
+        "MORNING_IMAGE":"https://ik.imagekit.io/jgp5dmcfb/Day-night/morning.png",
+        "EVENING_IMAGE":"https://ik.imagekit.io/jgp5dmcfb/Day-night/evening.png",
+        "STANDARD_IMAGE":"https://ik.imagekit.io/jgp5dmcfb/New_Iconic_Sheets_Set/1._Iconic_Sheet_Set_4pc_Light_Gray_Stack_2.jpg"
+
+    },
+    {
+        "skus":["BedSheetSet-Full-Light-Gray-FBA","M-BEDSHEETSET-F-LIGHT-GRAY-CMB","M-BEDSHEETSET-F-LIGHT-GRAY-PAK"],
+        "MORNING_IMAGE":"https://ik.imagekit.io/jgp5dmcfb/Day-night/4pc_Light%20Gray_Daytime.png",
+        "EVENING_IMAGE":"https://ik.imagekit.io/jgp5dmcfb/Day-night/4pc_Light%20Gray_Night.jpeg",
+        "STANDARD_IMAGE":"https://ik.imagekit.io/jgp5dmcfb/New_Iconic_Sheets_Set/1._Iconic_Sheet_Set_4pc_Light_Gray_Stack_2.jpg"
+
+    }
+]
+
+
+
+
+
